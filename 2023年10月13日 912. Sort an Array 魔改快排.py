7cbcaa3054e6c90e8f865ab56d304f48b878b6de
Bill <147676288+bizi2016@users.noreply.github.com>
2023-10-13 19:27:36 -0400

    Add files via upload

---
+++ 2023年10月13日 912. Sort an Array 魔改快排.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        if len(nums) <= 1:
+            return nums
+
+        pivot = random.choice(nums)
+        left, mid, right = [], [], []
+
+        for i in nums:
+            if i < pivot: left.append(i)
+            elif i > pivot: right.append(i)
+            else: mid.append(i)
+        
+        return self.sortArray(left) + mid + self.sortArray(right)
